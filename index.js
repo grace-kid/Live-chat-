6613e9e78e2008572b483a9cfbfbcbfa1e0008eb
catamondoller@gmail.com <catamondoler@gmail.com>
2024-12-31 01:48:02 +0100

    Your commit message

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve the main HTML file with a GET request
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Serve static files like CSS, JavaScript, etc.
+app.use(express.static(path.join(__dirname, 'public')));
+
+let onlineUsers = {}; // Track online users
+
+
+
+// When a user connects
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    
+    // Add user to the online users list
+    onlineUsers[socket.id] = socket;
+    io.emit('updateOnlineUsers', Object.keys(onlineUsers)); // Broadcast online users list
+
+    // Handle receiving chat messages
+    socket.on('chatMessage', (msg) => {
+        // Broadcast message to all other users (excluding the sender)
+        socket.broadcast.emit('chatMessage', msg);
+    });
+
+    // When a user disconnects
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+        
+        // Remove user from online users list
+        delete onlineUsers[socket.id];
+        io.emit('updateOnlineUsers', Object.keys(onlineUsers)); // Broadcast updated online users list
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+
