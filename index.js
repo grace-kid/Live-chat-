d668e5ffee72d883bb1d7dea2f282268327cb040
catamondoller@gmail.com <catamondoler@gmail.com>
2025-01-01 21:35:46 +0100

    fix bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
-
+require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
@@ -43,7 +43,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
